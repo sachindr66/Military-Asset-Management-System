0fe5cfea156a62733ae17f5aeaef30ea90c36e30
sachindr66 <sachindrhsn@gmail.com>
2025-07-01 10:59:38 +0530

    update PORT

--- backend/index.js
+++ backend/index.js
@@ -43,7 +43,7 @@ app.use('/api/bases', baseRoutes)
 
 app.use(logError)
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.CUSTOM_PORT || 5000;
 
 const connectDB = async () => {
     try {
